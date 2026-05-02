bd5ed127781633226f1caf565d4f2773d3c5aeda
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-02 17:06:53 -0400

    powershell: fix windows-specific font install

--- config/powershell/setup/Install-Fonts.ps1
+++ config/powershell/setup/Install-Fonts.ps1
@@ -1,7 +1,17 @@
-Install-PSResource Fonts -Scope CurrentUser
-Import-Module Fonts
+if ($IsWindows -or $env:OS -eq "Windows_NT") {
+  $Fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 
-Get-ChildItem "$PSScriptRoot/../fonts/" | ForEach-Object {
-  $fonts = Get-ChildItem "$($_.FullName)" -Filter *.ttf | Select-Object -ExpandProperty FullName
-  Install-Fonts $fonts -Scope CurrentUser
+  Get-ChildItem "$PSScriptRoot/../fonts/" | ForEach-Object {
+    Get-ChildItem "$($_.FullName)" -Filter *.ttf | 
+      Select-Object -ExpandProperty FullName | 
+      ForEach-Object { $Fonts.CopyHere($_, 0x10) }
+  }
+} else {
+  Install-PSResource Fonts -Scope CurrentUser
+  Import-Module Fonts
+
+  Get-ChildItem "$PSScriptRoot/../fonts/" | ForEach-Object {
+    $fonts = Get-ChildItem "$($_.FullName)" -Filter *.ttf | Select-Object -ExpandProperty FullName
+    Install-Fonts $fonts -Scope CurrentUser
+  }
 }
