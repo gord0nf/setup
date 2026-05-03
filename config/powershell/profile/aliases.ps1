9ba4e496e4129ac15c1414679d5fc07634541cf9
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-03 00:41:25 -0400

    powershell: better set wallpaper

--- config/powershell/profile/aliases.ps1
+++ config/powershell/profile/aliases.ps1
@@ -87,19 +87,46 @@ Set-Alias ffox firefox
 
 # Change wallpaper --------------------------------------------------------------------------------
 
+# https://stackoverflow.com/a/9440226
 Add-Type -TypeDefinition @"
+using System;
 using System.Runtime.InteropServices;
-public class wallpaper
+using Microsoft.Win32;
+namespace Wallpaper
 {
- public const int SetDesktopWallpaper = 20;
- public const int UpdateIniFile = 0x01;
- public const int SendWinIniChange = 0x02;
- [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-  private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
- public static void SetWallpaper ( string path )
- {
-  SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
- }
+   public enum Style : int
+   {
+       Tile, Center, Stretch, NoChange
+   }
+   public class Setter {
+      public const int SetDesktopWallpaper = 20;
+      public const int UpdateIniFile = 0x01;
+      public const int SendWinIniChange = 0x02;
+      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+      public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+         SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+         switch( style )
+         {
+            case Style.Stretch :
+               key.SetValue(@"WallpaperStyle", "2") ; 
+               key.SetValue(@"TileWallpaper", "0") ;
+               break;
+            case Style.Center :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "0") ; 
+               break;
+            case Style.Tile :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "1") ;
+               break;
+            case Style.NoChange :
+               break;
+         }
+         key.Close();
+      }
+   }
 }
 "@
 $ThemeDir = "$env:APPDATA\Microsoft\Windows\Themes"
@@ -109,7 +136,7 @@ function Set-Wallpaper()
   param ( [string]$Path )
   Remove-Item "$ThemeDir\TranscodedWallpaper" -ErrorAction SilentlyContinue
   Copy-Item "$Path" "$ThemeDir\TranscodedWallpaper"
-  [wallpaper]::SetWallpaper("$Path") # Refresh wallpaper
+  [Wallpaper.Setter]::SetWallpaper("$(Resolve-Path "$Path"")", 1) # Refresh wallpaper
 }
 
 function Set-RandomWallpaper()
