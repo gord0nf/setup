3e8027dc0baec01ac2dd5208d84d5fb584a4d2b5
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-02 21:38:45 -0400

    powershell: check if fonts are installed first

--- config/powershell/setup/Install-Fonts.ps1
+++ config/powershell/setup/Install-Fonts.ps1
@@ -1,8 +1,17 @@
+
 if ($IsWindows -or $env:OS -eq "Windows_NT") {
   $Fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+  $InstalledFonts = $Fonts.Items() | Select-Object -ExpandProperty Name
 
-  Get-ChildItem "$PSScriptRoot/../fonts/" | ForEach-Object {
-    Get-ChildItem "$($_.FullName)" -Filter *.ttf | 
+  $FontsToInstall = Get-ChildItem "$PSScriptRoot/../fonts" |
+    Select-Object -ExpandProperty Name |
+    Where-Object {
+      $fontName = $_
+      $InstalledFonts.Where({ $_ -like "$fontName*" }).Count -eq 0
+    }
+
+  $FontsToInstall | ForEach-Object {
+    Get-ChildItem "$PSScriptRoot/../fonts/$_" -Filter *.ttf | 
       Select-Object -ExpandProperty FullName | 
       ForEach-Object { $Fonts.CopyHere($_, 0x10) }
   }
