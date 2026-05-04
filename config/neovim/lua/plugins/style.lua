25a6b594c00e67e3792034b4329f0d9181b15acc
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-04 07:38:58 -0400

    neovim: nvim-highlight-colors

--- config/neovim/lua/plugins/style.lua
+++ config/neovim/lua/plugins/style.lua
@@ -11,7 +11,6 @@ return {
         italic_inlayhints = true,
         underline_links = true,
         disable_nvimtree_bg = true,
-        terminal_colors = true,
         color_overrides = {
           vscLineNumber = '#FFFFFF',
         },
@@ -71,5 +70,50 @@ return {
         },
       },
     },
+  },
+  { 'brenoprata10/nvim-highlight-colors' },
+  {
+    'saghen/blink.cmp',
+    opts = function(_, opts)
+      vim.tbl_deep_extend('force', opts, {
+        completion = {
+          menu = {
+            draw = {
+              components = {
+                -- customize the drawing of kind icons
+                kind_icon = {
+                  text = function(ctx)
+                    -- default kind icon
+                    local icon = ctx.kind_icon
+                    -- if LSP source, check for color derived from documentation
+                    if ctx.item.source_name == 'LSP' then
+                      local color_item =
+                        require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                      if color_item and color_item.abbr ~= '' then
+                        icon = color_item.abbr
+                      end
+                    end
+                    return icon .. ctx.icon_gap
+                  end,
+                  highlight = function(ctx)
+                    -- default highlight group
+                    local highlight = 'BlinkCmpKind' .. ctx.kind
+                    -- if LSP source, check for color derived from documentation
+                    if ctx.item.source_name == 'LSP' then
+                      local color_item =
+                        require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
+                      if color_item and color_item.abbr_hl_group then
+                        highlight = color_item.abbr_hl_group
+                      end
+                    end
+                    return highlight
+                  end,
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
   },
 }
