a79bc58790ba867f58904c1d1fa10109e83ea18f
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-04 20:57:02 -0400

    neovim: flash

--- config/neovim/lua/plugins/code.lua
+++ config/neovim/lua/plugins/code.lua
@@ -5,7 +5,6 @@ return {
     version = '*',
 
     ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
       fuzzy = { implementation = 'lua' }, -- should be 'prefer_rust' but Windows doesn't like it...
       signature = { enabled = true },
@@ -106,5 +105,56 @@ return {
         cond = symbols.has,
       })
     end,
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      modes = {
+        char = { jump_labels = true },
+      },
+    },
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
+    },
   },
 }
