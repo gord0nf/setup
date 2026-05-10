d337599d5129ca4e3cdf558375a3b7e4638f2b1b
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-10 15:19:10 -0400

    powershell: force option for font install

--- config/powershell/setup/Install-Fonts.ps1
+++ config/powershell/setup/Install-Fonts.ps1
@@ -1,15 +1,18 @@
+param ( [switch]$Force )
 
 if ($IsWindows -or $env:OS -eq "Windows_NT")
 {
   $Fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
   $InstalledFonts = $Fonts.Items() | Select-Object -ExpandProperty Name
 
-  $FontsToInstall = Get-ChildItem "$PSScriptRoot/../fonts" |
-    Select-Object -ExpandProperty Name |
-    Where-Object {
+  $FontsToInstall = Get-ChildItem "$PSScriptRoot/../fonts" | Select-Object -ExpandProperty Name
+  if (!$Force)
+  {
+    $FontsToInstall = $FontsToInstall | Where-Object {
       $fontName = $_
       $InstalledFonts.Where({ $_ -like "$fontName*" }).Count -eq 0
     }
+  }
 
   $FontsToInstall | ForEach-Object {
     Get-ChildItem "$PSScriptRoot/../fonts/$_" -Filter *.ttf | 
