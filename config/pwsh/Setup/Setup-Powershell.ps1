92185b295fa96b22b19bbdeb22ef4476e192528b
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-05-16 18:52:11 -0400

    pwsh: move all setup logic to Setup/

--- config/pwsh/Setup/Setup-Powershell.ps1
+++ config/pwsh/Setup/Setup-Powershell.ps1
@@ -2,7 +2,7 @@ Set-PSRepository PSGallery -InstallationPolicy Trusted
 Set-PSResourceRepository PSGallery -Trusted
 
 Write-Host 'Installing fonts'
-& "$PSScriptRoot/../fonts/Install-Fonts.ps1"
+& "$PSScriptRoot/fonts/Install-Fonts.ps1"
 
 Write-Host 'Installing icons'
 Install-Module Terminal-Icons -Repository PSGallery -Scope CurrentUser
