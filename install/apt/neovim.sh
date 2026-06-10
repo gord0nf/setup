997a26c95965ef28345011cec07f9bc55e87a71f
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-10 06:36:00 -0400

    neovim: fix apt reinstalling

--- install/apt/neovim.sh
+++ install/apt/neovim.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+THING=neovim
+source "$(dirname "${BASH_SOURCE[0]}")/../../utils.sh" || {
+  echo "fatal: couldn't source utils" >&2
+  exit 1
+}
+
+command_exists nvim && {
+  log 'already installed'
+  exit
+}
+
 $APT install -y make neovim
