e7a64c89622402968e9a36908dc8c1080ba94104
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-10 05:22:19 -0400

    sway: start behaviour

--- config/sway.sh
+++ config/sway.sh
@@ -15,3 +15,12 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh" || {
 [[ $EUID -eq 0 ]] && sway_dir=/etc/sway || sway_dir=$HOME/.config/sway
 log "creating directory link from '$sway_dir' to config"
 make_directory_link "$CONFIG" "$sway_dir"
+
+# extra configuration with env vars ---------------------------------------------------------------
+
+log 'applying yaml configuration stuff'
+
+# startBehavior should be login|prompt|manual
+[[ -v ymlconf_config_sway_startBehavior ]] &&
+  set_global_env SWAY_START "$ymlconf_config_sway_startBehavior" ||
+  set_global_env SWAY_START -unset
