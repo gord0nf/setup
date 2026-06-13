956976a8523c0565296b04bf4fc8a6675837ff65
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-12 21:11:20 -0400

    foot: dynamic themes

--- config/foot.sh
+++ config/foot.sh
@@ -13,3 +13,11 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh" || {
 foot_dir=${XDG_CONFIG_HOME:-$HOME/.config}/foot
 log "creating directory link from '$foot_dir' to config"
 make_directory_link "$CONFIG" "$foot_dir"
+
+# extra configuration with env vars ---------------------------------------------------------------
+
+if [[ -v ymlconf_config_foot_theme ]]; then
+  ln -s "$CONFIG/themes/$ymlconf_config_foot_theme.ini" "$CONFIG/theme.ini"
+else
+  touch "$CONFIG/theme.ini" # blank file so include directive in foot.ini doesn't fail
+fi
