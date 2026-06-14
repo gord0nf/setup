524407b98f91c08f2a096765dd58cf9e7124b8ca
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-14 01:42:08 -0400

    tmux: use tpm

--- config/tmux.sh
+++ config/tmux.sh
@@ -10,12 +10,23 @@ source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh" || {
 
 ! $FORCE && ! command_exists tmux && fatal 'not installed'
 
+# install/update tpm
+tpm_dir=~/.tmux/plugins/tpm
+[[ -d $tpm_dir ]] && {
+  log 'updating tpm'
+  git -C $tpm_dir pull
+} || {
+  log 'installing tpm'
+  rm -fr $tpm_dir # TODO: maybe a check here (if not $FORCE)?
+  git clone https://github.com/tmux-plugins/tpm $tpm_dir
+}
+
 # create link tmux_dir to config
-tmux_dir="${XDG_CONFIG_HOME:-~/.config}/tmux"
+tmux_dir="${XDG_CONFIG_HOME:-$HOME/.config}/tmux"
 log "creating directory link from '$tmux_dir' to config"
 make_directory_link "$CONFIG" "$tmux_dir"
 
 # make sure ~/.tmux.conf is the entry point that sources config
 log 'making sure ~/.tmux.conf sources'
 src_line="source-file $tmux_dir/tmux.conf"
-grep -qxF "$src_line" ~/.tmux.conf || echo "$src_line" >>~/.tmux.conf
+grep -qxF "$src_line" ~/.tmux.conf &>/dev/null || echo "$src_line" >>~/.tmux.conf
