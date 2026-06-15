6e0e4ec80f4a94355fec0d4ffd6b2ed53f3c9a22
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-14 23:44:00 -0400

    add alacritty

---
+++ config/alacritty.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FORCE="${FORCE:-false}"
+THING=alacritty
+CONFIG="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/$THING"
+source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh" || {
+  echo "fatal: couldn't source utils" >&2
+  exit 1
+}
+
+! $FORCE && ! command_exists nvim && fatal 'not installed'
+
+default_dirs=(
+  "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty"
+  "${APPDATA:+$APPDATA/alacritty}"
+)
+
+# create link from default nvim dir(s) to config
+for dir in "${default_dirs[@]}"; do
+  if [[ ! -z "$dir" && -d "$(dirname "$dir")" ]]; then
+    log "creating directory link from '$dir' to config"
+    make_directory_link "$CONFIG" "$dir"
+  fi
+done
