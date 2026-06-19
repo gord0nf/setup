219d5ea2cad3a09099703370f06cc3095ddd835f
gord0nf <264331696+gord0nf@users.noreply.github.com>
2026-06-19 02:10:57 -0400

    sway: start on any tty

--- config/sway/scripts/profile.sh
+++ config/sway/scripts/profile.sh
@@ -2,7 +2,7 @@
 
 # Sway launch behavior to be run in ~/.profile or equivalent
 
-if [[ -v SWAY_START && "$(tty)" == "/dev/tty1" ]]; then
+if [[ -v SWAY_START && "$(tty)" =~ /dev/tty[0-9] ]]; then
   start_sway="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/start-sway.sh"
   if [[ "$SWAY_START" == 'prompt' ]]; then
     read -p "Start Sway? [y]es or [n]o: " -n 1 -r -t 1 reply && echo
